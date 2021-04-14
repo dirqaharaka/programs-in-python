f9f1237c94e6404056164060c2247743cc4acbc1
dirqaharaka <54818623+dirkaharaka99@users.noreply.github.com>
2021-04-13 21:35:35 -0700

    Add files via upload

---
+++ km_ke_miles.py
@@ -0,0 +1,9 @@
+#Program untuk mengubah nilai dari km ke miles
+km = int(input("Masukkan nilai dalam kilometer: "))
+
+#1 km = 0.621371 Mil
+rasio = 0.621371
+
+mil = km * rasio
+
+print("Hasil nilai setelah diubah dalam Miles: ",mil)
